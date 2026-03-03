c7a12b9676dd00257cd717e40e24c43abe5be7a7
patinya phunphon <67160016@go.buu.ac.th>
2026-03-03 23:45:08 +0700

    PostList1

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,19 +2,20 @@ import PostCard from "./PostCard";
 import PostCount from "./PostCount";
 
 function PostList({ posts }) {
+  console.log(posts);
   return (
     <div>
       <h2
         style={{
           color: "#2d3748",
-          borderBottom: "2px solid #f1e40af",
+          borderBottom: "2px solid #1e40af",
           paddingBottom: "0.5rem",
         }}
       >
         โพสต์ล่าสุด
       </h2>
 
-      {/* ✅ ต้องอยู่ใน component */}
+      {/* ✅ ใส่ตรงนี้ */}
       <PostCount count={posts.length} />
 
       {posts.map((post) => (
